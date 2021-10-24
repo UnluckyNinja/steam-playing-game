e923c975eed7aae40dfdc16730852d3fba371bb0
UnluckyNinja <UnluckyNinja@users.noreply.github.com>
2021-10-24 16:06:43 +0800

    feat: setup demo & vercel serverless

---
+++ api/corsproxy.ts
@@ -0,0 +1,112 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+// We support the GET, POST, HEAD, and OPTIONS methods from any origin,
+// and allow any header on requests. These headers must be present
+// on all responses to all CORS preflight requests. In practice, this means
+// all responses to OPTIONS requests.
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+  "Access-Control-Max-Age": "86400",
+}
+
+// The rest of this snippet for the demo page
+function rawHtmlResponse(html) {
+  return new Response(html, {
+    headers: {
+      "content-type": "text/html;charset=UTF-8",
+    },
+  })
+}
+
+async function handleRequest(request: VercelRequest) {
+  const { url: apiUrl } = request.query;
+  const url = new URL(request.url, `http://${request.headers.host}`)
+  if (!apiUrl) {
+    return new Response(null, {status: 400, statusText: 'param "url" not defined'})
+  }
+
+  // Rewrite request to point to API url. This also makes the request mutable
+  // so we can add the correct Origin header to make the API server think
+  // that this request isn't cross-site.
+  const newRequest = new Request(apiUrl as string, request as any)
+  newRequest.headers.set("Origin", new URL(apiUrl as string).origin)
+  let response = await fetch(newRequest)
+
+  // Recreate the response so we can modify the headers
+  response = new Response(response.body, response)
+
+  // Set CORS headers
+  response.headers.set("Access-Control-Allow-Origin", url.origin)
+
+  // Append to/Add Vary header so browser will cache response correctly
+  response.headers.append("Vary", "Origin")
+
+  return response
+}
+
+function handleOptions(request: VercelRequest) {
+  // Make sure the necessary headers are present
+  // for this to be a valid pre-flight request
+  const headers = request.headers;
+  if (
+    headers["origin"] !== null &&
+    headers["access-control-request-method"] !== null &&
+    headers["access-control-request-headers"] !== null
+  ){
+    // Handle CORS pre-flight request.
+    // If you want to check or reject the requested method + headers
+    // you can do that here.
+    const respHeaders = {
+      ...corsHeaders,
+    // Allow all future content Request headers to go back to browser
+    // such as Authorization (Bearer) or X-Client-Name-Version
+      "Access-Control-Allow-Headers": request.headers["access-control-request-headers"],
+    }
+
+    return new Response(null, {
+      headers: respHeaders,
+    })
+  }
+  else {
+    // Handle standard OPTIONS request.
+    // If you want to allow other HTTP Methods, you can do that here.
+    return new Response(null, {
+      headers: {
+        Allow: "GET, HEAD, POST, OPTIONS",
+      },
+    })
+  }
+}
+
+const PROXY_ENDPOINT = "/api/corsproxy/"
+
+export default (request: VercelRequest, response: VercelResponse) => {
+  const { url: apiUrl } = request.query;
+
+  const url = new URL(request.url)
+  if(url.pathname.startsWith(PROXY_ENDPOINT)){
+    if (request.method === "OPTIONS") {
+      // Handle CORS preflight requests
+      const res = handleOptions(request)
+      const headers = Object.fromEntries(res.headers.entries())
+      response.writeHead(res.status,res.statusText, headers)
+      response.send(res.body)
+    }
+    else if(
+      request.method === "GET" ||
+      request.method === "HEAD" ||
+      request.method === "POST"
+    ){
+      // Handle requests to the API server
+      handleRequest(request).then(async (res)=>{
+        const headers = Object.fromEntries(res.headers.entries())
+        response.writeHead(res.status, res.statusText, headers)
+        response.json(await res.json())
+      })
+    }
+    else {
+      response.status(400).send('bad request')
+    }
+  }
+};
--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,96 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  <VueUseDemo />
+  <div>
+    <div v-if="!store.game && !store.key">鼠标悬停此处查看说明</div>
+    <span v-if="store.game">游戏：{{ store.game }}</span>
+  </div>
+  <div class="default-hide">
+    <div>
+      <div>
+        <label for="key">密钥:</label>
+        <input id="key" v-model="store.key" type="password" />
+      </div>
+      <div>
+        <label for="key">Steam 64位 ID:</label>
+        <input id="key" v-model="store.steamID" type="password" />
+      </div>
+      <div>
+        <label for="key">更新间隔（秒）</label>
+        <input
+          id="key"
+          :value="store.interval / 1000"
+          @input="changeInterval(Math.max(1000, $event.target.value * 1000))"
+          type="number"
+          autocomplete="off"
+        />
+      </div>
+    </div>
+    <h3>如何使用:</h3>
+    <p style="font-size: 1rem">(请不要在OBS内点击下方链接)</p>
+    <p style="color: red; font-size: 1rem">!! 下方链接会显示隐私信息，如果你正在直播，强烈建议暂时关闭相应捕捉直至完成 !!</p>
+    <ol>
+      <li>
+        打开
+        <a href="https://steamcommunity.com/dev/apikey">https://steamcommunity.com/dev/apikey</a>
+        获取API密钥，若没生成过则生成一个
+      </li>
+      <li>
+        64位 SteamID 可以在
+        <a href="https://steamdb.info/calculator/">https://steamdb.info/calculator/</a>
+        或
+        <a href="https://steamid.xyz/">https://steamid.xyz/</a>
+        获取
+      </li>
+      <li>
+        打开OBS等，添加浏览器源，填入本网页网址，宽度根据常玩的游戏名自动调整，高度100px足够，点击确定保存
+        <br />* 自定义参考CSS：
+        <pre>body { color: white; background-color: rgba(0, 0, 0, 0); margin: 0px auto; overflow-y: auto; }</pre>
+      </li>
+      <li>右键场景来源中对应项，点击倒数第三项：互动，在打开的窗口内填入对应内容，关闭即可</li>
+    </ol>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-import VueUseDemo from './components/VueUseDemo.vue'
+<script lang="ts" setup>
+import { useStorage } from '@vueuse/core';
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld,
-    VueUseDemo,
-  }
+const store = useStorage('store', {
+  message: '',
+  game: '',
+  key: '',
+  steamID: '',
+  interval: 5000
 })
+const fetchGame = async (key: string, steamID: string) => {
+  if (!key || !steamID) return ''
+  const res = await fetch(
+    '/api/corsproxy/?url='+encodeURIComponent('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=' +
+    store.value.key +
+    '&steamids=' +
+    store.value.steamID),
+    {
+      mode: 'cors',
+      method: 'GET'
+    }
+  )
+  return (await res.json()).response.players[0].gameextrainfo
+}
+
+function setupUpdate(store: any) {
+  function newUpdater() {
+    return setInterval(() => {
+      fetchGame(store.value.key, store.value.steamID)
+    }, store.value.interval)
+  }
+  let updaterID = newUpdater()
+  function changeInterval(value = 5000) {
+    clearInterval(updaterID)
+    store.value.interval = value
+    updaterID = newUpdater()
+  }
+  return { changeInterval }
+}
+
+const { changeInterval } = setupUpdate(store)
 </script>
 
 <style>
@@ -23,8 +98,25 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  max-width: 50vw;
+}
+
+#app .default-hide {
+  visibility: hidden;
+}
+#app:hover .default-hide {
+  visibility: visible;
+}
+body {
+  font-size: 1.5rem;
+  height: 100vh;
+  overflow: auto;
+}
+::-webkit-scrollbar {
+  width: 0; /* Remove scrollbar space */
+  background: transparent; /* Optional: just make scrollbar invisible */
+}
+pre {
+  white-space: pre-wrap;
 }
 </style>
